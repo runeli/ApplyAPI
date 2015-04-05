af4b35e16c289f218538eadd2e44d72dc203a454
runeli <aleksanteri.negru-vode@aalto.fi>
2015-04-05 21:30:38 +0300

    add writer configurations to the same config file

--- config.js
+++ config.js
@@ -6,10 +6,9 @@ module.exports = {
     showApplicantCount: true,
     pageDesc: [
         'One may use this API to instantaneously create an employment application server for people applying for technical positions. Applications are saved as .json files.',
-        
-        '<br /><br /> This is not a full-scale HR-management application, just an API with a descriptive web-interface',
-        '<br /><br /> See config.js for configurations.'
-    ].join(''),
+        'This is not a full-scale HR-management application, just an API with a descriptive web-interface',
+        'See config.js for configurations.'
+    ].join('<br /><br /> '),
     uploadDir: '/uploads',
     method: 'post',
     mime:'application/json',
@@ -35,8 +34,14 @@ module.exports = {
         },
         {
             type: String,
-            label: 'Location',
+            label: 'location',
             desc: 'Where are you located?',
+            required: false
+        },
+        {
+            type: Number,
+            label: 'age',
+            desc: 'How old are you?',
             required: false
         },
         {
@@ -51,5 +56,22 @@ module.exports = {
             desc: 'Do you need to work remotely?',
             required: false
         }
-    ]
+    ],
+
+    writer: {
+        fileType: 'json',  //File extension of the saved applications.
+        dest: '/uploads', //Folder to upload the applications.
+        locale: {
+            //Finnish time format. You can create your custom formats in similar manner
+            fin: function (date) {
+                if (!date) date = new Date();
+                return ('0' + date.getDate()).slice(-2) + ('0' + (date.getMonth() + 1)).slice(-2) + date.getFullYear();
+            }
+
+        },
+        fileNameFields: ['name', 'surname'], //Which fields should be visible in the application file.
+        separator: '_', //used to differentiate parts in saved filenames
+        saveMessage: 'Your application has been saved.' //The message displayed when the application has been successfully saved.
+
+    }
 }
